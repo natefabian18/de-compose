0b7df5ade1efa4ceafbee1bb3a99183ce536504f
Nate Fabian <nathan.fabian@stvincent.edu>
2020-10-28 21:22:16 -0400

    Created Battle Scene and basic button layout

---
+++ De-Composing/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+	public float offsetDistance;
+	public int buttonCount;
+
+	private int selected = 0;
+
+	//debug only
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A)) {
+			MoveLeft();
+		}
+		
+		if (Input.GetKeyDown(KeyCode.D)) {
+			MoveRight();
+		}
+	}
+
+	//debug only
+
+	public void MoveLeft() {
+		if (selected == 0) {
+			return;
+		}
+
+		this.transform.position -= new Vector3(offsetDistance, 0, 0);
+		selected--;
+	}
+
+	public void MoveRight() {
+		if (selected == buttonCount - 1) {
+			return;
+		}
+		this.transform.position += new Vector3(offsetDistance, 0, 0);
+		selected++;
+	}
+}
