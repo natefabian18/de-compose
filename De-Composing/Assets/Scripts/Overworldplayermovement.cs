3ae477468d1dd3a6da3d0892a74d7c6bc99b9d34
josh secrist <secrjosh@gmail.com>
2020-10-28 21:18:05 -0400

    Overworld player and enemy movement script created, generic overworld sprite made, overworld scene made

---
+++ De-Composing/Assets/Scripts/Overworldplayermovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overworldplayermovement : MonoBehaviour
+{
+    public Vector2 mapPosition;  //keeps track of player location on map when they transition back to overworld after combat
+    public float speed =  1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
