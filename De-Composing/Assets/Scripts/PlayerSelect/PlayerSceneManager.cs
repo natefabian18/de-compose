5818c3729a9a039da52634e2ddb9a2069dad7c7a
Nate Fabian <nathan.fabian@stvincent.edu>
2020-11-11 21:46:35 -0500

    Started Building Player Select Scene

---
+++ De-Composing/Assets/Scripts/PlayerSelect/PlayerSceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSceneManager : MonoBehaviour
+{
+	public int playersSelected = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void ButtonSelect(string selection) {
+		playersSelected++;
+
+		if (playersSelected <= 3) {
+			switch (selection) {
+				case "Trumpet":
+					Debug.Log("Trumpet");
+					break;
+				case "Drum":
+					Debug.Log("Drum");
+					break;
+				case "Maracas":
+					Debug.Log("Maracas");
+					break;
+				case "Xylophone":
+					Debug.Log("Xylophone");
+					break;
+				case "Harp":
+					Debug.Log("Harp");
+					break;
+				default:
+					Debug.LogError("button tossed invalid peram");
+					break;
+			}
+		}
+
+		if (playersSelected == 3) {
+			Debug.Log("load next scene");
+		}
+	}
+}
---
+++ De-Composing/Assets/Scripts/PlayerSelect/PlayerSelectButtons.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectButtons : MonoBehaviour
+{
+	public float offsetDistance;
+	public int buttonCount;
+
+	private int selected = 0;
+
+	private PlayerSceneManager playerSceneScript;
+
+	private bool[] taken;
+
+	//debug only
+
+	private void Start()
+	{
+		playerSceneScript = GameObject.FindGameObjectWithTag("PlayerSceneManager").GetComponent<PlayerSceneManager>();
+		taken = new bool[buttonCount];
+		for (int i = 0; i < taken.Length; i++) {
+			taken[i] = false;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			MoveLeft();
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			MoveRight();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			tossSelection();
+		}
+	}
+
+	//debug only
+
+	public void MoveLeft()
+	{
+		if (selected == 0)
+		{
+			return;
+		}
+
+		this.transform.position -= new Vector3(offsetDistance, 0, 0);
+		selected--;
+	}
+
+	public void MoveRight()
+	{
+		if (selected == buttonCount - 1)
+		{
+			return;
+		}
+		this.transform.position += new Vector3(offsetDistance, 0, 0);
+		selected++;
+	}
+
+	private void tossSelection()
+	{
+		if (taken[selected] == false)
+		{
+			switch (selected)
+			{
+				case 0:
+					GameObject.FindGameObjectWithTag("PlayerSceneManager");
+					playerSceneScript.ButtonSelect("Trumpet");
+					taken[selected] = true;
+					break;
+				case 1:
+					GameObject.FindGameObjectWithTag("PlayerSceneManager");
+					playerSceneScript.ButtonSelect("Harp");
+					taken[selected] = true;
+					break;
+				case 2:
+					GameObject.FindGameObjectWithTag("PlayerSceneManager");
+					playerSceneScript.ButtonSelect("Xylophone");
+					taken[selected] = true;
+					break;
+				case 3:
+					GameObject.FindGameObjectWithTag("PlayerSceneManager");
+					playerSceneScript.ButtonSelect("Drum");
+					taken[selected] = true;
+					break;
+				case 4:
+					GameObject.FindGameObjectWithTag("PlayerSceneManager");
+					playerSceneScript.ButtonSelect("Maracas");
+					taken[selected] = true;
+					break;
+				default:
+					Debug.LogError("no button selected");
+					break;
+			}
+		}
+	}
+}
