05c2b6e90d8eba6eb599314e296ab5c2127e31da
josh secrist <secrjosh@gmail.com>
2020-11-17 18:40:58 -0500

    Particle emitter added to lava, overworld enemy sprites added with colliders and tags for collisions

--- De-Composing/Assets/Scripts/Overworld/OverworldenemyAI.cs
+++ De-Composing/Assets/Scripts/Overworld/OverworldenemyAI.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class OverworldenemyAI : MonoBehaviour
 {
-    public Vector2 mapPosition;
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+
     public float speed = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = startPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 direction = new Vector2(transform.position.x - endPosition.x, transform.position.y - endPosition.y);
         
     }
 
