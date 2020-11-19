4cd8d87ec36842055fd5683ae607e47cda6eb63f
josh secrist <secrjosh@gmail.com>
2020-11-18 20:49:45 -0500

    Overworld enemy is moving, quip text added to boss battle and 2nd battle, new script boss dialog for changing dialog x amount of seconds after entering a fight

--- De-Composing/Assets/Scripts/Overworld/OverworldenemyAI.cs
+++ De-Composing/Assets/Scripts/Overworld/OverworldenemyAI.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class OverworldenemyAI : MonoBehaviour
 {
-    public Vector2 startPosition;
-    public Vector2 endPosition;
+    public GameObject startPosition;
+    public GameObject endPosition;
+
+	private Vector2 directionLeft;
+	private Vector2 directionRight;
+
+	private bool idleDirection = false; //true is left    false is right
 
     public float speed = 1f;
 
@@ -13,14 +18,36 @@ public class OverworldenemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = startPosition;
-    }
+		directionLeft = startPosition.transform.position - endPosition.transform.position;
+		directionLeft.Normalize();
+		directionRight = endPosition.transform.position - startPosition.transform.position;
+		directionRight.Normalize();
+	}
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = new Vector2(transform.position.x - endPosition.x, transform.position.y - endPosition.y);
-        
-    }
+		Vector2 directionToStart = startPosition.transform.position - transform.position;
+		Vector2 directionToEnd = endPosition.transform.position - transform.position;
+		float distanceToStart = directionToStart.magnitude;
+		float distanceToEnd = directionToEnd.magnitude;
+		if (!idleDirection && distanceToEnd > 2)
+		{
+			transform.position += new Vector3(directionRight.x, directionRight.y, 0) * speed * Time.deltaTime;
+		}
+		else if (!idleDirection && distanceToEnd < 2)
+		{
+			idleDirection = true;
+		}
+		else if (idleDirection && distanceToStart > 2)
+		{
+			transform.position += new Vector3(directionLeft.x, directionLeft.y, 0) * speed * Time.deltaTime;
+		}
+		else
+		{
+			idleDirection = false;
+		}
+
+	}
 
 }
