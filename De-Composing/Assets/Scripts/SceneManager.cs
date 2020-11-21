33026a7d9b9b4950823fb3285a123a858190f497
Nate Fabian <nathan.fabian@stvincent.edu>
2020-11-21 18:37:17 -0500

    Import partical emission code

---
+++ De-Composing/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    private ParticleSystem particles_snow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles_snow = GetComponent<ParticleSystem>();
+        particles_snow.Stop();
+        particles_snow.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("PARTICLES");
+    }
+}
